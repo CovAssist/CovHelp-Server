9ed6d1a2a6f53b9d59731ae3937c50e74cede38c
Parth <img_2019045@iiitm.ac.in>
2021-05-04 21:30:31 +0530

    added typescript

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express"
 const app = express();
 require("dotenv").config();
+app.get('/',(req,res)=>{res.send("Hello World")})
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running");
 });
