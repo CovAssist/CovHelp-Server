5ced32fc99d95dfd0da7192deec86b9e798278de
sejaljain123 <sejaljain2043@gmail.com>
2021-05-13 16:14:26 +0530

    MedicinesList

---
+++ src/controllers/Medicines/MedicinesLists.ts
@@ -0,0 +1,48 @@
+import { Request } from "express";
+import { AppResponse } from "../../@types";
+import { Medicine, IMedicines } from "../../models";
+export default async (req: Request, res: AppResponse) => {
+  if (req.query.verified === undefined)
+    return res.status(400).json({
+      message: "Please provide verification status",
+      error: true,
+    });
+
+  if (!req.query.start && !req.query.end)
+    return res.status(400).json({
+      message: "Please provide start and end parameters in query string",
+      error: true,
+    });
+
+  try {
+    let medicines: Array<IMedicines> = [];
+
+    if (req.query.name)
+      medicines = await Medicine.find({
+        name: `${req.query.name}`,
+        verified: req.query.verified === "true",
+      });
+    else
+      medicines = await Medicine.find({
+        verified: req.query.verified === "true",
+      });
+    const count = medicines.length;
+    const start = parseInt(req.query.start as string);
+    const end = parseInt(req.query.end as string);
+    if (end > count - 1) medicines = medicines.slice(start);
+    else medicines = medicines.slice(start, end + 1);
+    return res.status(200).json({
+      data: { data: medicines, count },
+      success: true,
+      message: "Result Success",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      data: { data: [], count: 0 },
+      success: false,
+      error: true,
+      message: "Server error",
+    });
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ import Volunteer from "./Volunteer";
 import Oxygen from "./Oxygen" 
 import Donor from "./Donor"
 import VacantBeds from "./VacantBeds";
+import Medicines from "./Medicines";
+
 const app=Router();
 
 app.use("/volunteer",Volunteer);
 app.use("/donor",Donor)
 app.use("/oxygen",Oxygen)
 app.use("/vacantbeds",VacantBeds)
+app.use("/medicines",Medicines)
 export default app;
